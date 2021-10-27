1bc544ef5ea4b8b2d4c5c54700b35d0301df1fcb
novel <novelbafagih@student.ub.ac.id>
2021-10-27 21:37:04 +0700

    Navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
-import { AppBar, Toolbar, Link } from "@mui/material"
-import { makeStyles } from "@mui/styles";
+import { AppBar, Toolbar, Link, Button, Stack } from "@mui/material"
+import { makeStyles, styled } from "@mui/styles";
 import Logo from "../assets/Logo.png";
 
 const useStyles = makeStyles((theme) => ({
 	logo:{
 		height:'35px'
-	}
+	},
+	boldText:{
+		fontWeight:900,
+	},
 }));
-
 export const Navbar = () =>{
 	const classes = useStyles();
 	return (
@@ -16,9 +18,29 @@ export const Navbar = () =>{
 			boxShadow:'none',
 		}}>
 			<Toolbar>
-				<Link href="https://udana.id/">
-					<img className={classes.logo} src={Logo}/>
-				</Link>
+
+				<Stack direction="row" spacing={2}>
+					<Link href="https://udana.id/">
+						<img className={classes.logo} src={Logo} alt="logo"/>
+					</Link>
+					<Link>
+						<Button sx={{color:"white",textTransform:'none',fontWeight:'900'}}>Wiralaba</Button>
+					</Link>
+					<Link>
+						<Button sx={{color:"white",textTransform:'none',fontWeight:'900'}}>FAQ</Button>
+					</Link>
+					<Link>
+						<Button sx={{color:"white",textTransform:'none',fontWeight:'900'}}>Tentang</Button>
+					</Link>
+				</Stack>
+				<Stack sx={{ml:"auto"}} direction="row" spacing={2}>
+					<Link>
+						<Button sx={{color:"white",textTransform:'none',fontWeight:'900'}}>Masuk</Button>
+					</Link>
+					<Link>
+						<Button variant="contained" sx={{textTransform:'none',fontWeight:'900'}}>Mulai Daftar</Button>
+					</Link>
+				</Stack>
 			</Toolbar>
 		</AppBar>
 	)
--- src/components/WelcomeSection.jsx
+++ src/components/WelcomeSection.jsx
@@ -1,9 +1,19 @@
+import { Box } from "@mui/material"
+import { makeStyles } from "@mui/styles";
 import { Navbar } from "./Navbar"
-
+const useStyles = makeStyles((theme) => ({
+	introImage:{
+		backgroundColor:"#000000",
+		height:"100vh"
+	}
+}));
 export const WelcomeSection = () =>{
+	const classes = useStyles();
 	return (
 		<>
-			<Navbar />
+			<Box className={classes.introImage}>
+				<Navbar />
+			</Box>
 		</>
 	)
 }
