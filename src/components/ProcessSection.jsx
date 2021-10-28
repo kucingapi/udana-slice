ce88c2a620b3078182e1bcbb1ffb3948db8ee23c
novel <novelbafagih@student.ub.ac.id>
2021-10-28 07:36:49 +0700

    making processSection

---
+++ src/components/ProcessSection.jsx
@@ -0,0 +1,14 @@
+import { Box, Grid } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+}));
+
+export const ProcessSection = () =>{
+
+	return(
+		<Box mt={15}>
+
+		</Box>
+	)
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
+import { ProcessSection } from "../components/ProcessSection"
 import { WelcomeSection } from "../components/WelcomeSection"
-
 export const Home = () =>{
 	return (
 		<>
 			<WelcomeSection/>
+			<ProcessSection/>
 		</>
 	)
 }
